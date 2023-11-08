baa36c947cb9f479ac8637b580dfa22eeca87596
FrancoMiralles98 <francobackend@gmail.com>
2023-11-07 23:41:57 -0300

    c2

--- src/funciones/prueba.js
+++ src/funciones/prueba.js
@@ -6,10 +6,13 @@ const cerebro = (str)=>{
     
  let ecc = findParentesis(str)
     
+console.log(ecc);
+ for (let index = 0,currentValue = 0, exc = ecc[0]; index < 1; index++) {
+ let segundoOrden = /[*/]/.test(exc)
+    if(segundoOrden){
+        let peticion = segundoOrdenFunction(exc)
 
- for (let index = 0,currentValue = 0; index < Infinity; index++) {
-    
-    
+    }
     
  }
 
@@ -19,8 +22,26 @@ console.timeEnd()
 
 
 
+    let segundoOrdenFunction = (ecc)=>{
+    
+        for (let index = 0; index < 1; index++) {
+            let indexMultipli = ecc.indexOf('*')
+            let indexDividir = ecc.indexOf('/')
+                if(indexMultipli > -1){
+                    
+                    let result = parseInt(ecc[indexMultipli -1]) * parseInt(ecc[indexMultipli+1])
+                    
+                }
+        }
+    }
 
-
+    let fixedEcc = (ecc,grado,symbol)=>{
+        if(grado = 2 && symbol=='*'){
+            let primerIndice = ecc.indexOf('+',ecc.indexOf('*')) > -1?ecc.indexOf('+',ecc.indexOf('*')) : ecc.indexOf('-',ecc.indexOf('*')) >-1? ecc.indexOf('-',ecc.indexOf('*')) : null
+            let segundoIndice = ecc.split('').reverse().join('').indexOf('+',ecc.indexOf('*')) > -1?ecc.split('').reverse().join('').indexOf('+',ecc.indexOf('*')) : ecc.split('').reverse().join('').indexOf('-',ecc.indexOf('*')) >-1? ecc.split('').reverse().join('').indexOf('-',ecc.indexOf('*')) : null
+        console.log(primerIndice,segundoIndice);
+        }
+    }
 
 
 let findParentesis = (str)=>{
@@ -35,4 +56,5 @@ let findParentesis = (str)=>{
 }
 
 
-console.log(cerebro('(1+1)*(2*2)'))
+//console.log(cerebro('(1+1*2)*(2*2)'))
+console.log(fixedEcc('1+1*2',2,'*'))
