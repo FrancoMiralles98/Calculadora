08b60d4c67a30fe6adfd023734e19b162892716d
FrancoMiralles98 <francobackend@gmail.com>
2023-11-08 00:26:56 -0300

    c4

--- src/funciones/prueba.js
+++ src/funciones/prueba.js
@@ -1,60 +1,25 @@
 console.time()
 
 
-const cerebro = (str)=>{
-    let temporalValue = 0
-    
- let ecc = findParentesis(str)
-    
-console.log(ecc);
- for (let index = 0,currentValue = 0, exc = ecc[0]; index < 1; index++) {
- let segundoOrden = /[*/]/.test(exc)
-    if(segundoOrden){
-        let peticion = segundoOrdenFunction(exc)
-
-    }
-    
- }
-
-}
-console.timeEnd()
-
-
-
-
-    let segundoOrdenFunction = (ecc)=>{
-    
-        for (let index = 0; index < 1; index++) {
-            let indexMultipli = ecc.indexOf('*')
-            let indexDividir = ecc.indexOf('/')
-                if(indexMultipli > -1){
-                    
-                    let result = parseInt(ecc[indexMultipli -1]) * parseInt(ecc[indexMultipli+1])
-                    
-                }
+const cerebro = (ecc) =>{
+let simbolos = ['+','-','*','/']
+let numero1= ''
+let numero2 = ''
+let simbolo = '+'
+let result = 0
+    for (let index = 0; index < ecc.length; index++) {
+        if(simbolos.includes(ecc[index])){
+            
         }
+        
+        
     }
 
-    let fixedEcc = (ecc,grado,symbol)=>{
-        if(grado = 2 && symbol=='*'){
-            let primerIndice = ecc.indexOf('+',ecc.indexOf('*')) > -1?ecc.indexOf('+',ecc.indexOf('*')) : ecc.indexOf('-',ecc.indexOf('*')) >-1? ecc.indexOf('-',ecc.indexOf('*')) : ecc.length
-            let segundoIndice = ecc.split('').lastIndexOf('+',primerIndice)
-        console.log(primerIndice,segundoIndice);
-        }
-    }
-
-
-let findParentesis = (str)=>{
     
-    let existParentesis = str.split('').indexOf('(',0)
 
-    if(existParentesis > -1){
-        console.log('hola');
-        let ecc = str.split('').slice((str.split('').indexOf('(',0))+1,str.split('').indexOf(')',0))
-         return [ecc.join(''),str.split('').indexOf('(',0),str.split('').indexOf(')',0)+1]
-    }
+    console.log(numero1);
+    console.log(numero2);
+    console.log(simbolo);
 }
 
-
-//console.log(cerebro('(1+1*2)*(2*2)'))
-console.log(fixedEcc('1+1+1*2',2,'*'))
+console.log(cerebro('111+111'));
