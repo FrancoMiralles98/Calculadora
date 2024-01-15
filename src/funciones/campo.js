925a939e2fbf82bf7b45a7d674ca63a7c12a20b2
FrancoMiralles98 <francobackend@gmail.com>
2024-01-15 17:48:58 -0300

    c21

--- src/funciones/campo.js
+++ src/funciones/campo.js
@@ -2,11 +2,11 @@ export function resolucion (c){
     if(c.length == 1){return{numbers:c,index:0}}
 
     let index
-    let indexSymbols = [...c.matchAll(/\d-\d|[+/√^*]|(sin)|(cos)|(tan)/g)]
-    if(indexSymbols.findIndex(e=>e[0] =='cos'| e[0] =='sin'| e[0] =='tan')>-1){let result = operacionTrigonometrica(c,index = c.index)}
-    if(indexSymbols.findIndex(e=>e[0] =='√'| e[0] =='^') > -1){console.log('hola primero');}
-    if(indexSymbols.findIndex(e=>e[0] =='/'| e[0] =='*') > -1){console.log('hola segundo');}
-    if(indexSymbols.findIndex(e=>e[0].indexOf('-') > -1| e[0] =='+') > -1){console.log('hola tercero');}
+    let indexSymbols = [...c.join('').matchAll(/\d-\d|[+/√^*]|(sin)|(cos)|(tan)/g)]
+    if(indexSymbols.findIndex(e=>e[0] =='cos'| e[0] =='sin'| e[0] =='tan')>-1){let result = operacionTrigonometrica(c,index = (index = c.indexOf('sin')) > -1?index: (index = c.indexOf('cos')) > -1?index: (index = c.indexOf('tan')) > -1?index: -1);return result}
+    if(indexSymbols.findIndex(e=>e[0] =='√'| e[0] =='^') > -1){let result = aritmetica(c, index = (index= c.indexOf('√')) > -1?index : (index= c.indexOf('^')) > -1?index: -1);return result }
+    if(indexSymbols.findIndex(e=>e[0] =='/'| e[0] =='*') > -1){let result = aritmetica(c, index = (index= c.indexOf('*')) > -1?index : (index= c.indexOf('/')) > -1?index: -1);return result }
+    if(indexSymbols.findIndex(e=>e[0].indexOf('-') > -1| e[0] =='+') > -1){let result = aritmetica(c,index = (index= c.indexOf('-')) > -1?index : (index= c.indexOf('+')) > -1?index: -1);return result }
 }
 
 function aritmetica (c,index){
@@ -60,6 +60,8 @@ function operacionTrigonometrica (c,index){
     }
 
 
-console.log(resolucion('cos40'));
+console.log(resolucion(['cos','40']));
+
+
 
 //\
